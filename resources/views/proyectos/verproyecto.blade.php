638a3d531711e36c4cf01bb5c74b8690b0abd2a8
Bruno <bruno.henriquez1217@gmail.com>
2023-07-27 20:09:00 -0400

    Commit inicial

--- resources/views/proyectos/verproyecto.blade.php
+++ resources/views/proyectos/verproyecto.blade.php
@@ -13,26 +13,28 @@
             @endif
             <h1 class="text-2xl font-bold text-center mb-5"></h1>
 
-            <div class="flex flex-col sm:flex-row items-center justify-between mb-4">
+            <div class="flex flex-col items-center space-y-4 sm:space-y-0 sm:flex-row sm:justify-between mb-4">
                 {{-- Botón de descarga --}}
-                <div>
-                    <form method="GET" action="{{ route('archivos.descargarCarpeta', ['proyectoId' => $proyecto->id]) }}">
+                <div class="w-full sm:w-auto">
+                    <form method="GET" action="{{ route('archivos.descargarCarpeta', ['proyectoId' => $proyecto->id]) }}" class="w-full">
                         @csrf
-                        <button type="submit" class="bg-slate-400 hover:bg-slate-600 text-white font-bold py-2 px-4 rounded">
+                        <button type="submit" class="w-full sm:w-auto bg-slate-400 hover:bg-slate-600 text-white font-bold py-2 px-4 rounded">
                             <i class="fas fa-download mr-1"></i>
                         </button>
                     </form>
                 </div>
 
-                <form method="POST" action="{{ route('archivos.subir', ['proyecto' => $proyecto->id]) }}" enctype="multipart/form-data" class="flex flex-col sm:flex-row items-center">
-                    @csrf
-                    <input type="hidden" name="proyecto_id" value="{{ $proyecto->id }}">
-                    <label for="archivos" class="border border-gray-300 rounded cursor-pointer p-2">
-                        <i class="fas fa-cloud-upload-alt mr-1"></i> Subir Archivos
-                    </label>
-                    <input type="file" name="archivos[]" id="archivos" class="hidden" multiple>
-                    <button type="submit" class="ml-2 sm:ml-4">Cargar</button>
-                </form>
+                <div class="w-full sm:w-auto">
+                    <form method="POST" action="{{ route('archivos.subir', ['proyecto' => $proyecto->id]) }}" enctype="multipart/form-data" class="w-full flex flex-col sm:flex-row items-center">
+                        @csrf
+                        <input type="hidden" name="proyecto_id" value="{{ $proyecto->id }}">
+                        <label for="archivos" class="w-full sm:w-auto border border-gray-300 rounded cursor-pointer p-2">
+                            <i class="fas fa-cloud-upload-alt mr-1"></i> Subir Archivos
+                        </label>
+                        <input type="file" name="archivos[]" id="archivos" class="hidden" multiple>
+                        <button type="submit" class="w-full sm:w-auto ml-2 sm:ml-4">Cargar</button>
+                    </form>
+                </div>
 
                 @if ($errors->any())
                     <div id="error-messages" class="text-red-500 mt-2 sm:mt-0 sm:ml-4">
@@ -45,9 +47,11 @@
                 @endif
 
                 {{-- Botón Ver Colaboradores --}}
-                <a href="{{ route('proyectos.colaboradores', ['proyecto' => $proyecto->id]) }}" class="mt-4 mb-2 sm:mt-0 sm:mb-0 sm:ml-4 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
-                    Ver Colaboradores
-                </a>
+                <div class="w-full sm:w-auto">
+                    <a href="{{ route('proyectos.colaboradores', ['proyecto' => $proyecto->id]) }}" class="w-full sm:w-auto mt-4 mb-2 sm:mt-0 sm:mb-0 sm:ml-4 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+                        Ver Colaboradores
+                    </a>
+                </div>
             </div>
 
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg mt-4">
