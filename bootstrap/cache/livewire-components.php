d385fe81ea868ae843fb48110f8628d6725f18d6
Bruno <bruno.henriquez1217@gmail.com>
2023-08-03 01:46:54 -0400

    cache

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'agregar-colaborador' => 'App\\Http\\Livewire\\AgregarColaborador',
+  'archivo-item' => 'App\\Http\\Livewire\\ArchivoItem',
+  'colaborador-proyecto' => 'App\\Http\\Livewire\\ColaboradorProyecto',
+  'crear-proyecto' => 'App\\Http\\Livewire\\CrearProyecto',
+  'editar-proyecto' => 'App\\Http\\Livewire\\EditarProyecto',
+  'lista-archivo' => 'App\\Http\\Livewire\\ListaArchivo',
+  'modal-versiones' => 'App\\Http\\Livewire\\ModalVersiones',
+  'mostrar-alerta' => 'App\\Http\\Livewire\\MostrarAlerta',
+  'mostrar-proyectos' => 'App\\Http\\Livewire\\MostrarProyectos',
+  'reproductor-audio' => 'App\\Http\\Livewire\\ReproductorAudio',
+  'versiones-archivo' => 'App\\Http\\Livewire\\VersionesArchivo',
+);
